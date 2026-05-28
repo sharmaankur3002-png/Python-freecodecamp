354da265b40c2bedaae7f4329be95ea75fe80f4e
Ankur Sharma <sharmaankur3002@gmail.com>
2026-05-28 11:49:53 +0530

    ADD RPG Character.

---
+++ Python basic/08_RPG_Character_Lab.py
@@ -0,0 +1,42 @@
+full_dot = '●'
+empty_dot = '○'
+
+def create_character(name , strength,intelligence, charisma):
+    if not isinstance(name,str):
+        return "The character name should be a string"
+    if len(name) == 0:
+        return "The character should have a name"
+
+    if len(name)>10 :
+        return "The character name is too long"
+
+
+   
+
+    if " " in name :
+        return "The character name should not contain spaces"
+
+    if not isinstance(strength,int):
+        return "All stats should be integers"
+    
+    elif not isinstance (intelligence,int): 
+        return "All stats should be integers"
+    
+    elif not isinstance (charisma,int):
+        return "All stats should be integers"
+
+
+    if strength < 1 or intelligence<1 or charisma < 1 :
+        return "All stats should be no less than 1"
+
+    if strength > 4 or intelligence>4 or charisma >4 :
+        return "All stats should be no more than 4"
+    
+    if (strength+intelligence+charisma)!= 7 :
+        return "The character should start with 7 points"
+
+
+    return (f"""{name}\nSTR {strength *full_dot }{(10 - strength)*empty_dot}\nINT {intelligence *full_dot }{(10 - intelligence)*empty_dot}\nCHA {charisma *full_dot }{(10 - charisma)*empty_dot}""")
+a=create_character('Ankur', 1, 4, 2)
+
+print(a)
